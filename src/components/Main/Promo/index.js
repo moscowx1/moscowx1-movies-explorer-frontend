4155173a1745a59baeee2261dfaead690830543e
moscowx1 <moscowx1@mail.ru>
2021-07-01 04:07:44 +0500

    fix link

--- src/components/Main/Promo/index.js
+++ src/components/Main/Promo/index.js
@@ -1,28 +1,28 @@
-﻿import {Link} from "react-router-dom";
+﻿import { Link } from "react-router-dom";
 
 import world from '../../../images/world.png';
 import './index.css';
 
 const Promo = () => {
-    return (
-        <section className="promo">
-            <img src={world}
-                 className="promo__world-image"
-                 Falt="Мир веба" />
-            <div className="promo__info">
-                <h1 className="promo__title">
-                    Учебный проект студента факультета
-                    {' '}
-                    <span className="promo__span">Веб-разработки</span>
-                    .
-                </h1>
-                <p className="promo__desc">
-                    Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-                </p>
-            </div>
-            <Link className="promo__link">Узнать больше</Link>
-        </section>
-    );
+  return (
+    <section className="promo">
+      <img src={world}
+        className="promo__world-image"
+        alt="Мир веба" />
+      <div className="promo__info">
+        <h1 className="promo__title">
+          Учебный проект студента факультета
+          {' '}
+          <span className="promo__span">Веб-разработки</span>
+          .
+        </h1>
+        <p className="promo__desc">
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        </p>
+      </div>
+      <Link className="promo__link" to="#">Узнать больше</Link>
+    </section>
+  );
 }
 
 export default Promo;
