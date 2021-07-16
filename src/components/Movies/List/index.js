27f087499142dc39ba06a11cb9248977430830ba
moscowx1 <moscowx1@mail.ru>
2021-07-16 19:59:25 +0500

    added not found txt to movie search

--- src/components/Movies/List/index.js
+++ src/components/Movies/List/index.js
@@ -3,16 +3,21 @@
 import Movie from "./Movie";
 
 const List = ({ movies, handleBtnClick, movieVisible }) => {
+  const viewMovies = movies
+    .filter(movie => movie.visible)
+    .slice(0, movieVisible);
+
   return (
     <ul className="movies__list">
-      {movies
-      .filter(movie => movie.visible)
-      .slice(0, movieVisible)
-      .map((movie) => (
-        <Movie key={movie.id}
-          data={movie}
-          handleBtnClick={handleBtnClick} />
-      ))}
+      {viewMovies
+        .map((movie) => (
+          <Movie key={movie.id}
+            data={movie}
+            handleBtnClick={handleBtnClick} />
+        ))}
+        {movies && viewMovies.length === 0 &&
+            <p className='movies__not-found-txt'>Ничего не найдено?</p>
+        }
     </ul>
   );
 };
